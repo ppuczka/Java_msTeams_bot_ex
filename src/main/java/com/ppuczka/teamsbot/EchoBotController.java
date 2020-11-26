771eeddf16e3e6d08681c4ab5342463cb02e74e6
Przemysław Puczka <p.puczka@gmail.com>
2020-11-26 22:39:31 +0100

    Hero Card Template

--- src/main/java/com/ppuczka/teamsbot/EchoBotController.java
+++ src/main/java/com/ppuczka/teamsbot/EchoBotController.java
@@ -27,29 +27,6 @@ public class EchoBotController extends ActivityHandler {
                     "DevOps of the Day and also about DevOps team absence" +
                     "Type anything to get started";
 
-    private static final String INFOMESSAGE =
-            "You are seeing this message because the bot received at least one "
-                    + "'ConversationUpdate' event, indicating you (and possibly others) "
-                    + "joined the conversation. If you are using the emulator, pressing "
-                    + "the 'Start Over' button to trigger this event again. The specifics "
-                    + "of the 'ConversationUpdate' event depends on the channel. You can "
-                    + "read more information at: " + "https://aka.ms/about-botframework-welcome-user";
-
-    private static final String LOCALEMESSAGE =
-            "You can use the activity's GetLocale() method to welcome the user "
-                    + "using the locale received from the channel. "
-                    + "If you are using the Emulator, you can set this value in Settings.";
-
-    private static final String PATTERNMESSAGE =
-            "It is a good pattern to use this event to send general greeting"
-                    + "to user, explaining what your bot can do. In this example, the bot "
-                    + "handles 'hello', 'hi', 'help' and 'intro'. Try it now, type 'hi'";
-
-    private static final String FIRST_WELCOME_ONE =
-            "You are seeing this message because this was your first message ever to this bot.";
-
-    private static final String FIRST_WELCOME_TWO =
-            "It is a good practice to welcome the user and provide personal greeting. For example: Welcome %s";
 
     private final UserState userState;
     private final ConversationReferences references;
@@ -73,8 +50,8 @@ public class EchoBotController extends ActivityHandler {
                 ).map(channel -> turnContext.sendActivities(
                         MessageFactory.text("Hi there! " + WELCOME_MESSAGE),
                         MessageFactory.text(manOfTheDay + " is man of the day")))
-                        .collect(CompletableFutures.toFutureList())
-                        .thenApply(resourceResponses -> null);
+                .collect(CompletableFutures.toFutureList())
+                .thenApply(resourceResponses -> null);
     }
 
     @Override
@@ -92,13 +69,12 @@ public class EchoBotController extends ActivityHandler {
                     conversationData.setDidBotWelcomeUser(true);
 
                     userProfile.setName(turnContext.getActivity().getText());
-                    return turnContext.sendActivities(MessageFactory.text(FIRST_WELCOME_ONE),
-                        MessageFactory.text(String.format(FIRST_WELCOME_TWO, userProfile.getName())));
-
-               } else {
+                    HeroCard introCard = createIntroCard(userProfile.getName());
+                    return turnContext.sendActivity(MessageFactory.attachment(introCard.toAttachment()));
+                } else {
                     conversationData.setUserPromptedForName(true);
                     conversationData.setDidBotWelcomeUser(true);
-                    return turnContext.sendActivity(MessageFactory.text("What is your name"));
+                    return turnContext.sendActivity(MessageFactory.text("What is your name ?"));
                 }
             } else {
                 conversationData.setUserPromptedForName(true);
@@ -121,19 +97,14 @@ public class EchoBotController extends ActivityHandler {
 
     public static String getManOfTheDay() {
         DayOfWeek dayOfWeek = LocalDateTime.now().getDayOfWeek();
-        switch (dayOfWeek) {
-            case MONDAY:
-                return "Ewa";
-            case TUESDAY:
-                return "Tomek";
-            case WEDNESDAY:
-                return "Wojtek";
-            case THURSDAY:
-                return "Przemek";
-            case FRIDAY:
-                return "Zbyszek";
-            default: return "Invalid date";
-        }
+        return switch (dayOfWeek) {
+            case MONDAY -> "Ewa";
+            case TUESDAY -> "Tomek";
+            case WEDNESDAY -> "Wojtek";
+            case THURSDAY -> "Przemek";
+            case FRIDAY -> "Zbyszek";
+            default -> "Invalid date";
+        };
     }
 
     @Override
@@ -147,15 +118,10 @@ public class EchoBotController extends ActivityHandler {
         references.put(conversationReference.getUser().getId(), conversationReference);
     }
 
-    private CompletableFuture<ResourceResponse> sendIntroCard(TurnContext turnContext) {
+    private static HeroCard createIntroCard(String userName) {
         HeroCard card = new HeroCard() {{
-            setTitle("Welcome to Bot Framework!");
-            setText(
-                    "Welcome to Welcome Users bot sample! This Introduction card "
-                            + "is a great way to introduce your Bot to the user and suggest "
-                            + "some things to get them started. We use this opportunity to "
-                            + "recommend a few next steps for learning more creating and deploying bots."
-            );
+            setTitle(String.format("Welcome to Bot Framework !", userName));
+            setText("What do You wand to do ?");
         }};
 
         card.setImages(Collections.singletonList(new CardImage() {
@@ -166,7 +132,7 @@ public class EchoBotController extends ActivityHandler {
 
         card.setButtons(Arrays.asList(
                 new CardAction() {{
-                    setType(ActionTypes.OPEN_URL);
+                    setType(ActionTypes.POST_BACK);
                     setTitle("Get an overview");
                     setText("Get an overview");
                     setDisplayText("Get an overview");
@@ -192,7 +158,6 @@ public class EchoBotController extends ActivityHandler {
                 }})
         );
 
-        Activity response = MessageFactory.attachment(card.toAttachment());
-        return turnContext.sendActivity(response);
+        return card;
     }
 }
